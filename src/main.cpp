de10931ab3519ac81aa32c273d05b3a9fc014005
imaerials <arielro85@gmail.com>
2025-06-06 17:45:25 -0300

    Add DHT sensor integration to web server; display temperature and humidity data

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,27 @@
 #include <WiFiManager.h>
 #include <WebServer.h> // For ESP32
+#include <DHT.h>
+
+#define DHTPIN 4 // GPIO04
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
 
 WebServer server(80); // Create web server on port 80
 
 void handleRoot() {
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
     String html = "<html><head><title>Wine Fridge Info</title></head><body>";
     html += "<h1>Wine Fridge Device Info</h1>";
     html += "<p><b>IP Address:</b> " + WiFi.localIP().toString() + "</p>";
     html += "<p><b>SSID:</b> " + WiFi.SSID() + "</p>";
     html += "<p><b>Signal Strength (RSSI):</b> " + String(WiFi.RSSI()) + " dBm</p>";
+    if (isnan(h) || isnan(t)) {
+        html += "<p><b>DHT11 Sensor:</b> Error reading data</p>";
+    } else {
+        html += "<p><b>Temperature:</b> " + String(t) + " &deg;C</p>";
+        html += "<p><b>Humidity:</b> " + String(h) + " %</p>";
+    }
     html += "</body></html>";
     server.send(200, "text/html", html);
 }
@@ -29,6 +42,7 @@ void setup() {
         Serial.print("Local IP: ");
         Serial.println(WiFi.localIP());
     }
+    dht.begin();
     // Start web server after WiFi is connected
     server.on("/", handleRoot);
     server.begin();
