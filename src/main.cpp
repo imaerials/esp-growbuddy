06619301e2f837a4a759d92a505641cc561725bf
imaerials <arielro85@gmail.com>
2025-06-06 17:41:19 -0300

    Refactor WiFiManager and WebServer integration; add web server functionality to display device info

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,17 @@
 #include <WiFiManager.h>
+#include <WebServer.h> // For ESP32
+
+WebServer server(80); // Create web server on port 80
+
+void handleRoot() {
+    String html = "<html><head><title>Wine Fridge Info</title></head><body>";
+    html += "<h1>Wine Fridge Device Info</h1>";
+    html += "<p><b>IP Address:</b> " + WiFi.localIP().toString() + "</p>";
+    html += "<p><b>SSID:</b> " + WiFi.SSID() + "</p>";
+    html += "<p><b>Signal Strength (RSSI):</b> " + String(WiFi.RSSI()) + " dBm</p>";
+    html += "</body></html>";
+    server.send(200, "text/html", html);
+}
 
 void setup() {
     Serial.begin(115200);
@@ -16,8 +29,13 @@ void setup() {
         Serial.print("Local IP: ");
         Serial.println(WiFi.localIP());
     }
+    // Start web server after WiFi is connected
+    server.on("/", handleRoot);
+    server.begin();
+    Serial.println("Web server started. Open http://" + WiFi.localIP().toString());
 }
 
 void loop() {
+    server.handleClient(); // Handle web server requests
     // Main code here
 }
